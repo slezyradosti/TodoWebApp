8c597ff0d6b117b70b730bdea9d2561859a7d7df
Life is squid <rredzer000@gmail.com>
2024-01-20 22:26:16 +0000

    added seeds

---
+++ Repository/EFInitial/Seed.cs
@@ -0,0 +1,47 @@
+namespace Repository.EFInitial;
+
+public static class Seed
+{
+    public static async Task SeedData(DataContext context)
+    {
+        var taskList = new List<Models.Task>();
+
+        var rnd = new Random();
+
+        if (!context.Task.Any())
+        {
+            taskList =
+            [
+                new()
+                {
+                    Details = "Praise the Role",
+                    IsDone = true
+                },
+
+                new()
+                {
+                    Details = "Break the law",
+                    IsDone = false
+                },
+
+                new()
+                {
+                    Details = "Take what's mine",
+                    IsDone = false
+                },
+                new()
+                {
+                    Details = "Take some more",
+                    IsDone = false
+                }
+            ];
+            await context.Task.AddRangeAsync(taskList);
+        }
+        else
+        {
+            taskList = context.Task.ToList();
+        }
+        
+        await context.SaveChangesAsync();
+    }
+}
