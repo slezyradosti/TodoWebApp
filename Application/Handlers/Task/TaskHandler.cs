2bb915334f70bbb5fce5df43e458ea51d9b43687
Life is squid <rredzer000@gmail.com>
2024-01-20 17:43:14 +0000

    task handler main actions

--- Application/Handlers/Task/TaskHandler.cs
+++ Application/Handlers/Task/TaskHandler.cs
@@ -1,12 +1,90 @@
-﻿namespace Application.Handlers.Task
+﻿using Application.Core;
+using Application.DTOs;
+using AutoMapper;
+using Repository.Repos.Interfaces;
+
+namespace Application.Handlers.Task
 {
     public class TaskHandler : ITaskHandler
     {
-        public TaskHandler()
+        private readonly ITaskRepository _taskRepository;
+        private readonly IMapper _mapper;
+
+        public TaskHandler(ITaskRepository taskRepository, IMapper mapper)
         {
-            
+            _taskRepository = taskRepository;
+            _mapper = mapper;
         }
 
+        public async Task<Result<List<TaskDto>>> GetTaskListAsync()
+        {
+            var taskList = await _taskRepository.GetAllAsync();
 
+            List<TaskDto> taskDtoList = new List<TaskDto>();
+
+            _mapper.Map(taskList, taskDtoList);
+
+            return Result<List<TaskDto>>.Success(taskDtoList);
+        }
+        
+        public async Task<Result<TaskDto>> GetTaskAsync(Guid taskId)
+        {
+            var taskList = await _taskRepository.GetOneAsync(taskId);
+
+            TaskDto taskDto = new TaskDto();
+
+            _mapper.Map(taskList, taskDto);
+            
+            return Result<TaskDto>.Success(taskDto);
+        }
+        
+        public async Task<Result<string>> CreateTaskAsync(TaskDto taskDto)
+        {
+            var task = new Models.Task();
+            
+            _mapper.Map(taskDto, task);
+
+            var result = await _taskRepository.AddAsync(task) > 0;
+
+            if (!result) return Result<string>.Failure("Failed to create Task");
+            return Result<string>.Success("Successfully");
+        }
+        
+        public async Task<Result<string>> EditTaskAsync(TaskDto taskDto)
+        {
+            var task = await _taskRepository.GetOneAsync(taskDto.Id);
+            if (task == null) return null;
+            
+            _mapper.Map(taskDto, task);
+
+            var result = await _taskRepository.SaveAsync(task) > 0;
+
+            if (!result) return Result<string>.Failure("Failed to update Task");
+            return Result<string>.Success("Successfully");
+        }
+        
+        public async Task<Result<string>> DeleteTaskAsync(Guid taskId)
+        {
+            var task = await _taskRepository.GetOneAsync(taskId);
+            if (task == null) return null;
+
+            var result = await _taskRepository.RemoveAsync(task) > 0;
+            
+            if (!result) return Result<string>.Failure("Failed to delete Task");
+            return Result<string>.Success("Successfully");
+        }
+        
+        public async Task<Result<string>> MarkTaskAsync(Guid taskId, bool isDone)
+        {
+            var task = await _taskRepository.GetOneAsync(taskId);
+            if (task == null) return null;
+
+            task.IsDone = isDone;
+
+            var result = await _taskRepository.SaveAsync(task) > 0;
+
+            if (!result) return Result<string>.Failure("Failed to update Task");
+            return Result<string>.Success("Successfully");
+        }
     }
 }
