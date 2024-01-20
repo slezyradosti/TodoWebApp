d3bd3b3bebb564f5ba0487cc84a0e5112ae37893
Life is squid <rredzer000@gmail.com>
2024-01-20 18:13:38 +0000

    removed Title property from TaskDto as well

--- Application/DTOs/TaskDto.cs
+++ Application/DTOs/TaskDto.cs
@@ -2,7 +2,6 @@
 {
     public class TaskDto : BaseDto
     {
-        public string Title { get; set; }
         public string Details { get; set; }
         public bool IsDone { get; set; } = false;
     }
